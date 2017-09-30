b1d40161934ac041fbfb9ec9b81590e8fdec3e19
tttnns <titansnow@outlook.com>
2017-09-30 19:46:31 +0800

    try theme diktat

--- conf.py
+++ conf.py
@@ -7,18 +7,4 @@ author = 'TitanSnow'
 language = 'zh_CN'
 exclude_patterns = ['_build']
 html_theme_path = ['.']
-html_theme = 'alab'
-html_static_path = ['_static']
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'searchbox.html',
-    ]
-}
-html_theme_options = {
-    'font_family': 'serif',
-    'code_font_family': '"Fira Mono", monospace',
-    'head_font_family': 'sans-serif',
-    'body_text_align': 'justify',
-}
+html_theme = 'diktat'
