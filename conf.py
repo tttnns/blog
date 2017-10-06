b4a2dd0895c452162b63ccae392b87fe7ed25eb8
tttnns <titansnow@outlook.com>
2017-10-06 16:42:15 +0800

    set master_color

--- conf.py
+++ conf.py
@@ -7,4 +7,7 @@ author = 'TitanSnow'
 language = 'zh_CN'
 exclude_patterns = ['_build']
 html_theme = 'diktat'
+html_theme_options = {
+    'master_color': '#212121',
+}
 html_short_title = 'tttnns 的博客'
